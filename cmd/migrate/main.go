c34d0a849fb13b59496676f0b21703e5c74bf15f
Gautam Singh <gautamsingh12122003@gmail.com>
2026-08-26 01:47:52 +0530

    add: migrate functionality for db

---
+++ cmd/migrate/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gautam1228/ZeroGo/internal/config"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("usage: migrate <up | down>")
+	}
+
+	// Loading config to access db url
+	cfg := config.MustLoad()
+
+	m, err := migrate.New(
+		"file://migrations",
+		cfg.DatabaseUrl)
+	if err != nil {
+		log.Fatalf("migrate.new: %v", err)
+	}
+
+	switch os.Args[1] {
+	case "up":
+		if err := m.Up(); err != nil {
+			log.Fatal(err)
+		}
+	case "down":
+		if err := m.Down(); err != nil {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatalf("unknown command: %s", os.Args[1])
+	}
+
+	fmt.Println("Migration Applied successfully ...")
+}
